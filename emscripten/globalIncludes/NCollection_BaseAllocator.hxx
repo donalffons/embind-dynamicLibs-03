65f8e3e4885dca9a4e249ad1c09c61d6f4c226ec
Sebastian Alff <donalffons@gmail.com>
2020-12-18 19:08:46 +0100

    simplified

--- emscripten/globalIncludes/NCollection_BaseAllocator.hxx
+++ emscripten/globalIncludes/NCollection_BaseAllocator.hxx
@@ -2,7 +2,7 @@
 #define NCollection_BaseAllocator_HeaderFile
 
 #include <Standard_Transient.hxx>
-#include <Standard_Type.hxx>
+#include <Standard_Handle.hxx>
 
 class NCollection_BaseAllocator : public Standard_Transient
 {
