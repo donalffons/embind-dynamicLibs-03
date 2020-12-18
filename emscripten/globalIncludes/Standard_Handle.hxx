16ac0052c727293e70348a23db88ee0ce151bde4
Sebastian Alff <donalffons@gmail.com>
2020-12-18 19:30:00 +0100

    simplified

--- emscripten/globalIncludes/Standard_Handle.hxx
+++ emscripten/globalIncludes/Standard_Handle.hxx
@@ -1,23 +1,15 @@
 #ifndef _Standard_Handle_HeaderFile
 #define _Standard_Handle_HeaderFile
 
-#include <Standard_Transient.hxx>
-
 template <class T>
 class handle
 {
 public:
+  handle () {}
 
-  //! Empty constructor
-  handle () : entity(0) {}
-
-  //! Constructor from pointer to new object
-  handle (const T *thePtr) : entity(const_cast<T*>(thePtr))
+  handle (const T *thePtr) 
   {
   }
-
-private:
-  Standard_Transient* entity;
 };
 
 #define Handle(Class) handle<Class>
